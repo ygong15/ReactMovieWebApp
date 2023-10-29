fb1a9ae5ae3346ec92c3741c34978b32432faa13
Yuchen (Aaron) Gong <gycaaron@outlook.com>
2023-10-29 01:35:12 -0700

    react basics complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {useState} from "react";
+import { useEffect } from 'react';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100)
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello, React</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}> - </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}> + </button>
     </div>
   );
 }
